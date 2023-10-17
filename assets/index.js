0ec1ac7903b56e605b58562c46809ba3c156d712
Miguel Ilek dos Santos <miguelsantosilek@gmail.com>
2023-10-17 22:27:44 +0100

    lógica das naves descendo

--- assets/index.js
+++ assets/index.js
@@ -8,22 +8,46 @@ function num(max) {
 
 function addAlienShip() {
 
-    const alienShip = `<figure class="${distanceLeft[num(6)]} alien-ship going-down">
-                     <img src="assets/imgs/nav alien voando.png">
-                     <p>${phonemes[num(32)]}</p>
-                   </figure>`;
+    var alienShip = `<figure id="0" class="noventa alien-ship going-down">
+                        <img src="assets/imgs/nav alien voando.png">
+                        <p>a</p>
+                    </figure>`
 
+    for (let a = 1; a < 200; a++) {
+        alienShip += `<figure id="${a}" class="${distanceLeft[num(6)]} alien-ship going-down">
+                        <img src="assets/imgs/nav alien voando.png">
+                        <p>${phonemes[num(32)]}</p>
+                      </figure>`;
+    }
+    
     gameContent.innerHTML += alienShip;
+}
+
+
+var delay = 0;
+
+addAlienShip()
+
+for (let x = 0; x <= 199; x++) {
+    let alienShip = document.getElementById(`${x}`);
+    alienShip.style.animationDelay = `${delay}s`;
+    delay = delay + 8;
+    if(delay > 100){
+        delay = delay - 6;
+    }else if(delay > 80){
+        delay = delay - 4;
+    }else if(delay > 60){
+        delay = delay - 2;
+    }else if(delay > 40){
+        delay = delay - 1;
+    }
 
 }
 
-var playEstate = 1;
 
-while(playEstate == 1){
-    addAlienShip();
-    
-    
-}
+
+
+
 
 
 
